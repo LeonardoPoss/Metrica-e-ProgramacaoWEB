4da42db7a2317e84ff71b564738abed1ec8e6eb1
LeonardoPoss <135801343+LeonardoPoss@users.noreply.github.com>
2024-04-10 14:48:20 -0300

    Add files via upload

---
+++ Calculadora IMC/aplicacao/src/CalculadoraImc.php
@@ -0,0 +1,18 @@
+<?php
+
+class CalculadoraImc
+{
+    private Usuario $usuario;
+
+    public function __construct(Usuario $usuario)
+    {
+        $this->usuario = $usuario;
+    }
+
+
+    public function calcular(): float
+    { 
+        $resultado = number_format($this->usuario->getPeso() / ($this->usuario->getAltura() * $this->usuario->getAltura()), 2);
+        return $resultado;
+    }
+}
