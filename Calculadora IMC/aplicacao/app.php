98a378b777e4bcf106e91e7bc07390cb993e57fd
LeonardoPoss <135801343+LeonardoPoss@users.noreply.github.com>
2024-04-15 16:17:38 -0300

    Update app.php

--- Calculadora IMC/aplicacao/app.php
+++ Calculadora IMC/aplicacao/app.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/src/Usuario.php';
 require_once __DIR__ . '/src/CalculadoraImc.php';
 require_once __DIR__ . '/src/SexoEnum.php';
 require_once __DIR__ . '/src/ClassificacaoImcEnum.php';
+require_once __DIR__ . '/src/logs_de_erro/log_erro.txt';
 
 
 $usuario = new Usuario( nome: $_POST['nome'], 
